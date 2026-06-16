4139f816660f6564dbc9392b8c667231e0d3b17d
janickimaciej <maciej.m.janicki@gmail.com>
2026-06-16 12:21:13 +0200

    Simplify shader program class

--- src/shaderProgram.cpp
+++ src/shaderProgram.cpp
@@ -53,11 +53,6 @@ void ShaderProgram::setUniform(const std::string& name, bool value) const
 	glUniform1i(glGetUniformLocation(m_id, name.c_str()), static_cast<int>(value));
 }
 
-void ShaderProgram::setUniform(const std::string& name, int value) const
-{
-	glUniform1i(glGetUniformLocation(m_id, name.c_str()), value);
-}
-
 void ShaderProgram::setUniform(const std::string& name, float value) const
 {
 	glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
@@ -76,23 +71,6 @@ void ShaderProgram::setUniform(const std::string& name, const glm::vec2& value) 
 void ShaderProgram::setUniform(const std::string& name, const glm::vec3& value) const
 {
 	glUniform3fv(glGetUniformLocation(m_id, name.c_str()), 1, glm::value_ptr(value));
-}
-
-void ShaderProgram::setUniform(const std::string& name, const glm::vec4& value) const
-{
-	glUniform4fv(glGetUniformLocation(m_id, name.c_str()), 1, glm::value_ptr(value));
-}
-
-void ShaderProgram::setUniform(const std::string& name, const glm::mat3& value) const
-{
-	glUniformMatrix3fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE,
-		glm::value_ptr(value));
-}
-
-void ShaderProgram::setUniform(const std::string& name, const glm::mat4& value) const
-{
-	glUniformMatrix4fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE,
-		glm::value_ptr(value));
 }
 
 ShaderProgram::ShaderProgram(const std::vector<std::string>& shaderPaths,
